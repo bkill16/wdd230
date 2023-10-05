dd3dc799e016754bd46edcf7b7ea0251569e4f6d
Brenleigh Killpack <kil21008@byui.edu>
2023-10-05 10:17:27 -0600

    dark mode fix

--- home-page/scripts/darkMode.js
+++ home-page/scripts/darkMode.js
@@ -2,14 +2,5 @@ const modeButton = document.querySelector("#mode");
 const body = document.querySelector("body");
 
 modeButton.addEventListener("click", () => {
-	if (modeButton.textContent.includes("☑️")) {
-		body.style.background = "#121212";
-		body.style.color = "#FFFFFF";
-		modeButton.textContent = "❎";
-        
-	} else {
-		body.style.background = "#FFFFFF";
-		body.style.color = "#121212";
-		modeButton.textContent = "☑️";
-	}
+	body.classList.toggle("darkMode");
 });
