33a1b6a9d3d55b96d5b39a30fa363ce6c05d9823
Brenleigh Killpack <kil21008@byui.edu>
2023-10-17 14:49:45 -0600

    page count fix

--- home-page/scripts/pageVisits.js
+++ home-page/scripts/pageVisits.js
@@ -1,10 +1,6 @@
 let numVisits = localStorage.getItem('numVisits');
 
-if (numVisits == 0) {
-  numVisits.textContent = `This is your first visit. Welcome!`;
-} else {
-  numVisits.textContent = numVisits;
-}
+numVisits.textContent = numVisits
 
 document.getElementById('visits').textContent = numVisits;
 
