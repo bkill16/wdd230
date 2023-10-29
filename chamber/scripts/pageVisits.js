1fad29a79c245f88d27483ec6828876f0ea87843
Brenleigh Killpack <kil21008@byui.edu>
2023-10-28 19:18:52 -0600

    page visits

---
+++ chamber/scripts/pageVisits.js
@@ -0,0 +1,17 @@
+if (localStorage.getItem('lastVisit')) {
+    const lastDate = new Date(localStorage.getItem('lastVisit'));
+    const currentDate = new Date();
+    const timeDifference = currentDate - lastDate;
+    const daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+
+    if (daysDifference < 1) {
+        document.querySelector('#pageVisits').textContent = "Back so soon? Awesome!";
+    } else if (daysDifference === 1) {
+        document.querySelector('#pageVisists').textContent = "You last visited 1 day ago.";
+    } else {
+        document.querySelector('#pageVisits').textContent = `You last visited ${daysDifference} ago.`;
+    }
+} else {
+    document.querySelector('#pageVisits').textContent = "Welcome! Let us know if you have any questions."
+    localStorage.setItem('lastVisit', new Date().toString());
+}
