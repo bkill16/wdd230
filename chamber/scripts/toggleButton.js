200435ec4c3dc5b31259442140ee65a6687acbd2
bkill16 <kil21008@byui.edu>
2023-11-17 14:51:50 -0700

    directory done

--- chamber/scripts/toggleButton.js
+++ chamber/scripts/toggleButton.js
@@ -1,16 +1,25 @@
-const gridBtn = document.querySelector("#grid-btn");
-const listBtn = document.querySelector("#list-btn");
-const display = document.querySelector(".directory-grid");
-
-gridBtn.addEventListener("click", showGrid);
-listBtn.addEventListener("click", showList);
-
-function showGrid() {
-    display.classList.add("grid");
-    display.classList.remove("list");
-}
-
-function showList() {
-    display.classList.add("list");
-    display.classList.remove("grid");
-}
+document.addEventListener('DOMContentLoaded', function () {
+    const display = document.querySelector('.directory-grid');
+  
+    // Check if neither 'grid' nor 'list' is set, then set 'grid'
+    if (!display.classList.contains('grid') && !display.classList.contains('list')) {
+      display.classList.add('grid');
+    }
+  });
+  
+  const gridBtn = document.querySelector('#grid-btn');
+  const listBtn = document.querySelector('#list-btn');
+  const display = document.querySelector('.directory-grid');
+  
+  gridBtn.addEventListener('click', showGrid);
+  listBtn.addEventListener('click', showList);
+  
+  function showGrid() {
+    display.classList.add('grid');
+    display.classList.remove('list');
+  }
+  
+  function showList() {
+    display.classList.add('list');
+    display.classList.remove('grid');
+  }
