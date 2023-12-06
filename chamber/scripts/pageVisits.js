2fa5e8d9b160085ccf1582f0ee7ae82fdccbab68
bkill16 <kil21008@byui.edu>
2023-12-05 21:56:35 -0700

    fix

--- chamber/scripts/pageVisits.js
+++ chamber/scripts/pageVisits.js
@@ -1,17 +1,23 @@
-if (localStorage.getItem('lastVisit')) {
-    const lastDate = new Date(localStorage.getItem('lastVisit'));
-    const currentDate = new Date();
-    const timeDifference = currentDate - lastDate;
-    const daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+let lastVisit = localStorage.getItem('lastVisit');
 
-    if (daysDifference < 1) {
-        document.querySelector('#pageVisits').textContent = "Back so soon? Awesome!";
-    } else if (daysDifference === 1) {
-        document.querySelector('#pageVisits').textContent = "You last visited 1 day ago.";
-    } else {
-        document.querySelector('#pageVisits').textContent = `You last visited ${daysDifference} days ago.`;
-    }
-} else {
-    document.querySelector('#pageVisits').textContent = "Welcome! Let us know if you have any questions."
-    localStorage.setItem('lastVisit', new Date().toString());
+let message = "Welcome! Let us know if you have any questions.";
+
+if (lastVisit) {
+
+  lastVisit = new Date(lastVisit);
+  
+  const daysDiff = Math.floor((new Date() - lastVisit) / (1000 * 60 * 60 * 24));  
+
+  if(daysDiff < 1) {
+    message = "Back so soon? Awesome!";
+  } else if (daysDiff === 1){ 
+    message = "You last visited 1 day ago.";
+  } else {
+    message = `You last visited ${daysDiff} days ago.`;  
+  }
+
 }
+
+document.querySelector('#pageVisits').textContent = message;
+
+localStorage.setItem('lastVisit', new Date());
