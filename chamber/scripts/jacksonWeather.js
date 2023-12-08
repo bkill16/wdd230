317073c7acf70cc5dad51e30596078a25836ff40
bkill16 <kil21008@byui.edu>
2023-12-08 14:15:13 -0700

    done??

--- chamber/scripts/jacksonWeather.js
+++ chamber/scripts/jacksonWeather.js
@@ -1,6 +1,5 @@
 const currentTemp = document.querySelector("#current-temp");
-const weatherIcon = document.querySelector("#weather-icon");
-const captionDesc = document.querySelector("figcaption")
+const captionDesc = document.querySelector("#weather-desc")
 const url = "https://api.openweathermap.org/data/2.5/weather";
 const queryString = `?lat=43.61&lon=-110.70&units=imperial&appid=0fa0694f89080df75594ebdf2899dd0a`;
 const fullUrl = url + queryString;
@@ -22,10 +21,9 @@ async function apiFetch() {
 apiFetch();
 
 function displayResults(data) {
-    currentTemp.innerHTML = `${Math.round(data.main.temp)}&deg;F`;
-    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-    let desc = data.weather[0].description;
-    weatherIcon.setAttribute('src', iconsrc);
-    weatherIcon.setAttribute('alt', desc);
+    const temperature = `${Math.round(data.main.temp)}&deg;F`;
+    const desc = data.weather[0].description;
+
+    currentTemp.innerHTML = `Weather Today: ${temperature},&nbsp; `;
     captionDesc.textContent = `${desc}`;
 }
